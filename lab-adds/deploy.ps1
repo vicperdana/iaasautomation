065b0d2e1b6159c480204526512b41aae2ac6be8
vicky.esp <vicky.esp@gmail.com>
2019-03-13 21:12:33 +1100

    commit

---
+++ lab-adds/deploy.ps1
@@ -0,0 +1,74 @@
+﻿$startTime=Get-Date
+Write-Host "Beginning deployment at $starttime"
+
+Import-Module Azure -ErrorAction SilentlyContinue
+
+#DEPLOYMENT OPTIONS
+    $templateToDeploy        = "NoClientDeploy.json"
+    # MUST be unique for all your simultaneous/co-existing deployments of this ADName in the same region
+    $VNetAddrSpace2ndOctet   = "1"
+
+    # Must be unique for simultaneous/co-existing deployments
+    #"master" or "dev"
+    $RGName                  = "rg-addslab"
+    $DeployRegion            = "eastus"
+    $Branch                  = "master"
+    $AssetLocation           = "https://raw.githubusercontent.com/vicperdana/iaasautomation/master/lab-adds/"
+    $userName                = "vperdana"
+    $secpasswd               = 'Pass1234##!!'
+    $adDomainName            = "vperdana.lab"
+    $usersArray              = @(
+                                @{ "FName"= "Bob";  "LName"= "Jones";    "SAM"= "bjones" },
+                                @{ "FName"= "Bill"; "LName"= "Smith";    "SAM"= "bsmith" },
+                                @{ "FName"= "Mary"; "LName"= "Phillips"; "SAM"= "mphillips" },
+                                @{ "FName"= "Sue";  "LName"= "Jackson";  "SAM"= "sjackson" }
+                               )
+    $defaultUserPassword     = "P@ssw0rd"
+
+
+
+#END DEPLOYMENT OPTIONS
+
+
+#ensure we're logged in
+Get-AzureRmContext -ErrorAction Stop
+
+#deploy
+$parms=@{
+    "adminPassword"               = $secpasswd;
+    "adminUsername"               = $userName;
+    "adDomainName"                = $ADDomainName;
+    "assetLocation"               = $assetLocation;
+    "virtualNetworkAddressRange"  = "10.$VNetAddrSpace2ndOctet.0.0/16";
+    #The first IP deployed in the AD subnet, for the DC
+    "adIP"                        = "10.$VNetAddrSpace2ndOctet.1.4";
+    #The first ADFS server deployed in the AD subnet - multiple farms will increment beyond this
+    "adSubnetAddressRange"        = "10.$VNetAddrSpace2ndOctet.1.0/24";
+    "dmzSubnetAddressRange"       = "10.$VNetAddrSpace2ndOctet.2.0/24";
+    "cliSubnetAddressRange"       = "10.$VNetAddrSpace2ndOctet.3.0/24";
+    #if multiple deployments will need to route between vNets, be sure to make this distinct between them
+    "deploymentNumber"            = $VNetAddrSpace2ndOctet;
+    "usersArray"                  = $usersArray;
+    "defaultUserPassword"         = "P@ssw0rd";
+}
+
+$TemplateFile = "$($assetLocation)$templateToDeploy" + "?x=5"
+$templateFile
+
+try {
+    Get-AzureRmResourceGroup -Name $RGName -ErrorAction Stop
+    Write-Host "Resource group $RGName exists, updating deployment"
+}
+catch {
+    $RG = New-AzureRmResourceGroup -Name $RGName -Location $DeployRegion -Tag @{ Shutdown = "true"; Startup = "false"}
+    Write-Host "Created new resource group $RGName."
+}
+$version ++
+$deployment = New-AzureRmResourceGroupDeployment -ResourceGroupName $RGName -TemplateParameterObject $parms -TemplateFile $TemplateFile -Name "adfsDeploy$version"  -Force -Verbose
+
+
+$endTime=Get-Date
+
+Write-Host ""
+Write-Host "Total Deployment time:"
+New-TimeSpan -Start $startTime -End $endTime | Select Hours, Minutes, Seconds
