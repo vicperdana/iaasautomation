9effafc770a1bbf0d814e5c1d1e9294b02936d28
vicky.esp <vicky.esp@gmail.com>
2019-04-23 21:40:16 +1000

    commit

---
+++ lab-adds/Scripts/DisablePublicIP.ps1
@@ -0,0 +1,18 @@
+param (
+    [Parameter(Mandatory)]
+    [string]$VMName,
+
+    [Parameter(Mandatory)]
+    [string]$ResourceGroup
+
+)
+
+$ErrorActionPreference = "Stop"
+$azurevm = Get-AzureRMVM -ResourceGroupName $resGroupName -Name $vmName
+#Write-output "AzureVM variable :" $azurevm
+$NicId = $azurevm | foreach {$_.NetworkProfile.NetworkInterfaces.Id}
+#Write-output "NicId variable $NicId"
+$NicArmObject = Get-AzureRmResource -ResourceId $NicId
+$nic = Get-AzureRmNetworkInterface -Name $NicArmObject.Name -ResourceGroupName $NicArmObject.ResourceGroupName
+$nic.IpConfigurations.publicipaddress.id = $null
+Set-AzureRmNetworkInterface -NetworkInterface $nic
